401d25b65a34ec06ffccefca957f926d68560cc5
valerie <valeriea874@gmail.com>
2022-03-09 15:31:26 -0500

    last class

--- src/app/learning-typescript/other-typescript-stuff.spec.ts
+++ src/app/learning-typescript/other-typescript-stuff.spec.ts
@@ -19,6 +19,15 @@ describe('cool stuff in ts', () => {
         let boringSpread = myNums.map((eachNum: number) => isEven(eachNum * 2));
         //spreading content of myNums and adding 7,2,44
         let moreNums: number[] = [...myNums, 7, 2, 44];
+
+        function addTwoNumbers(parameter1: number, parameter2:number)
+     {
+        return parameter1 + parameter2;
+     }
+        let myTwoNumbers = [2, 3];
+        addTwoNumbers(myTwoNumbers[0], myTwoNumbers[1]);
+       // addTwoNumbers(...myTwoNumber)
+        
     });
 
     //copy from instructor that is exported
@@ -36,10 +45,37 @@ describe('cool stuff in ts', () => {
         
     });
 
-    it('should use the rest', () => {
+    it('should use the rest parameter', () => {
         // the rest operator
         // ...
-        
-    });
+        const myNums: number[] = [1,5,3,9];
 
-});
+        function addSomeNumbers(...theNumbers: number[])
+        // theNumbers would be an array
+        //answer would be 18 here 1 + 5 = 6 +3 = 9 + 9 = 18
+        {
+            return theNumbers.reduce((acc, current) =>
+             acc + current);
+        }
+        addSomeNumbers(2, 5, 8, 5);
+        addSomeNumbers(...myNums);
+        addSomeNumbers(3,2);
+
+        function simon(param1: number, param2: number, ...theRest: any[]){
+            console.log(param1)//7
+            console.log(param2)//4
+            console.log(theRest)//['taco', 'lizard', lee-roy']
+        }
+
+        simon(7, 4, 'taco', 'lizard', 'lee-roy');
+       
+    });
+    // it('destructuring', () => {
+    //     function giveMeTuple(): [number, string]
+    //     {
+    //         let random = M
+    //     }
+    // });
+
+
+//});
